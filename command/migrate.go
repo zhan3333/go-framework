e52c64c141a1a81e75da4e5192174c74807d565c
zhan <grianchan@gmail.com>
2020-02-01 19:55:57 +0800

    add migrate.go

---
+++ command/migrate.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+var action = flag.String("action", "", "操作类型")
+var name = flag.String("name", "", "文件名称")
+var saveDir = "database/migrations"
+
+func main() {
+	flag.Parse()
+	switch *action {
+	case "create":
+		create()
+	}
+}
+
+func create() {
+	var err error
+	var out []byte
+	fmt.Printf("create migrate: %s", *name)
+	fmt.Println()
+	cmd := exec.Command("/bin/bash", "-c", fmt.Sprintf("migrate create -ext sql -dir %s %s", saveDir, *name))
+	if out, err = cmd.Output(); err != nil {
+		fmt.Println(err)
+		fmt.Println(string(out))
+		os.Exit(1)
+	}
+	fmt.Println("create success")
+}
