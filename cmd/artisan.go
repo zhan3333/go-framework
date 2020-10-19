89b367a2333af14dc6b33bb98a336616510c14d7
zhan <grianchan@gmail.com>
2020-10-19 17:41:57 +0800

    change migrate step default 1 to 999

--- cmd/artisan.go
+++ cmd/artisan.go
@@ -6,6 +6,7 @@ import (
 	"github.com/zhan3333/go-migrate"
 	"go-framework/boot"
 	"os"
+	"strconv"
 )
 
 func init() {
@@ -28,13 +29,26 @@ func main() {
 			return
 		}
 		action := os.Args[2]
+		step := 0
+		if len(os.Args) >= 4 {
+			step, err = strconv.Atoi(os.Args[3])
+			if err != nil {
+				fmt.Printf("Err: step 参数必须为数字")
+			}
+		}
 		if action == "migrate" {
-			if err = migrate.Migrate(1); err != nil {
+			if step == 0 {
+				step = 999
+			}
+			if err = migrate.Migrate(step); err != nil {
 				fmt.Printf("migrate failed: %+v\n", err)
 			}
 		}
 		if action == "rollback" {
-			if err = migrate.Rollback(1); err != nil {
+			if step == 0 {
+				step = 1
+			}
+			if err = migrate.Rollback(step); err != nil {
 				fmt.Printf("rollback failed: %+v\n", err)
 			}
 		}
